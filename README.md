39247a844303cc07b77714bfbc5b3ac5e2277984
dwididit <didit@dwidi.com>
2024-08-31 18:10:40 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Spring Boot Reactive gRPC Application
+
+This project is a Spring Boot application that provides a gRPC service for managing users. It is built using Spring Boot, gRPC, and PostgreSQL, and demonstrates how to create a reactive gRPC service with persistence using Spring Data JPA.
+
+## Features
+
+- gRPC service with CRUD operations for user management
+- Integration with PostgreSQL for data persistence
+- Reflection-enabled gRPC service for easy interaction with tools like Postman
+- Reactive programming model using Spring WebFlux
+
+## Prerequisites
+
+- Java 21
+- Maven 3.9.8
+- PostgreSQL
+
+## Getting Started
+
+### Clone the Repository
+
+```bash
+gh repo clone dwididit/spring-boot-reactive-grpc
+cd spring-boot-reactive-grpc/
+```
--- src/main/java/dev/dwidi/springbootreactivegrpc/SpringBootReactiveGrpcApplication.java
+++ src/main/java/dev/dwidi/springbootreactivegrpc/SpringBootReactiveGrpcApplication.java
@@ -1,7 +1,14 @@
 package dev.dwidi.springbootreactivegrpc;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.protobuf.services.ProtoReflectionService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class SpringBootReactiveGrpcApplication {
@@ -10,4 +17,22 @@ public class SpringBootReactiveGrpcApplication {
         SpringApplication.run(SpringBootReactiveGrpcApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner startGrpcServer() {
+        return args -> {
+            Server server = ServerBuilder.forPort(9090)
+                    .addService(new UserServiceImpl())
+                    .addService(ProtoReflectionService.newInstance())
+                    .build();
+
+            Logger logger = Logger.getLogger(SpringBootReactiveGrpcApplication.class.getName());
+            logger.info("Starting gRPC server...");
+            server.start();
+            logger.info("gRPC server started, listening on " + server.getPort());
+
+            Runtime.getRuntime().addShutdownHook(new Thread(server::shutdown));
+
+            server.awaitTermination();
+        };
+    }
 }
